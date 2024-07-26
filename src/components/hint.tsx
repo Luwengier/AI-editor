9b75c9414982720c4a3d4a2bee578b8019e8cad7
Luewngier <g217881111@gmail.com>
2024-07-26 16:44:05 +0800

    Create hint component

---
+++ src/components/hint.tsx
@@ -0,0 +1,31 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+
+export interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  side?: 'top' | 'right' | 'bottom' | 'left';
+  align?: 'start' | 'center' | 'end';
+  sideOffset?: number;
+  alignOffset?: number;
+}
+
+export const Hint = ({ label, children, ...rest }: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={100}>
+        <TooltipTrigger asChild>{children}</TooltipTrigger>
+        <TooltipContent
+          className="text-white bg-slate-800 border-slate-800"
+          {...rest}
+        >
+          <p className="font-semibold capitalize">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
