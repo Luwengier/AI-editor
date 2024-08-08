11b05dd088c099a956fedc0fd3c808166824e2d8
Luewngier <g217881111@gmail.com>
2024-08-08 16:51:10 +0800

    Set up background removal api route

--- src/app/api/[[...route]]/ai.ts
+++ src/app/api/[[...route]]/ai.ts
@@ -4,36 +4,57 @@ import { zValidator } from '@hono/zod-validator';
 
 import { replicate } from '@/lib/replicate';
 
-const app = new Hono().post(
-  '/generate-image',
-  zValidator(
-    'json',
-    z.object({
-      prompt: z.string(),
-    })
-  ),
-  async (c) => {
-    const { prompt } = c.req.valid('json');
+const app = new Hono()
+  .post(
+    '/remove-bg',
+    zValidator('json', z.object({ image: z.string() })),
+    async (c) => {
+      const { image } = c.req.valid('json');
 
-    const input = {
-      prompt: prompt,
-      num_outputs: 1,
-      aspect_ratio: '1:1',
-      output_format: 'webp',
-      output_quality: 90,
-    };
+      const input = {
+        image: image,
+      };
 
-    const output: unknown = await replicate.run(
-      'black-forest-labs/flux-schnell',
-      {
-        input,
-      }
-    );
+      const output: unknown = await replicate.run(
+        'cjwbw/rembg:fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003',
+        { input }
+      );
 
-    const res = output as string[];
+      const res = output as string;
 
-    return c.json({ data: res[0] });
-  }
-);
+      return c.json({ data: res });
+    }
+  )
+  .post(
+    '/generate-image',
+    zValidator(
+      'json',
+      z.object({
+        prompt: z.string(),
+      })
+    ),
+    async (c) => {
+      const { prompt } = c.req.valid('json');
+
+      const input = {
+        prompt: prompt,
+        num_outputs: 1,
+        aspect_ratio: '1:1',
+        output_format: 'webp',
+        output_quality: 90,
+      };
+
+      const output: unknown = await replicate.run(
+        'black-forest-labs/flux-schnell',
+        {
+          input,
+        }
+      );
+
+      const res = output as string[];
+
+      return c.json({ data: res[0] });
+    }
+  );
 
 export default app;
