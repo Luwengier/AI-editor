51acc969a053f089b24c69ff33ed26fc7b2238e8
Luewngier <g217881111@gmail.com>
2024-08-21 13:24:07 +0800

    Add google o-auth provider

--- src/auth.ts
+++ src/auth.ts
@@ -1,13 +1,15 @@
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github';
+import Google from 'next-auth/providers/google';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
 
 import { db } from '@/db/drizzle';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Github],
+  providers: [Github, Google],
   pages: {
     signIn: '/sign-in',
+    error: '/sign-in',
   },
 });
