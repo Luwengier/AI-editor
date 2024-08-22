7e660d6e2365db2f3983617e2b34295567fcac3e
Luewngier <g217881111@gmail.com>
2024-08-22 14:43:31 +0800

    Set up JWT strategy and credentials providers

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,56 @@
 import NextAuth from 'next-auth';
 import Github from 'next-auth/providers/github';
 import Google from 'next-auth/providers/google';
+import Credentials from 'next-auth/providers/credentials';
 import { DrizzleAdapter } from '@auth/drizzle-adapter';
+import { JWT } from 'next-auth/jwt';
 
 import { db } from '@/db/drizzle';
 
+declare module 'next-auth/jwt' {
+  interface JWT {
+    id: string | undefined;
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [Github, Google],
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        console.log({ credentials });
+
+        return null;
+      },
+    }),
+    Github,
+    Google,
+  ],
   pages: {
     signIn: '/sign-in',
     error: '/sign-in',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id;
+      }
+
+      return session;
+    },
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+
+      return token;
+    },
   },
 });
